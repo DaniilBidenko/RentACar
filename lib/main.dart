e8e71f9bca66e35f12c6f179a5e9ad4b70f0289c
DaniilBidenko <daniilbidenko5@gmail.com>
2025-07-17 08:54:42 +0600

    Начало проекта

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main () {
+  runApp(RentACar());
+}
+
+class RentACar extends StatefulWidget {
+  RentACar({Key? key}) : super(key: key);
+  @override
+  State<RentACar> createState() => _RentACarState();
+}
+
+class _RentACarState extends State<RentACar> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
